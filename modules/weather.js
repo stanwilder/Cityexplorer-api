d63c47a7aba7535130d69f8850b845ddeb51a3e6
Stanley <stanley.wilder.24@gmail.com>
2022-05-18 18:21:23 -0700

    app crashed checking to see if i can push and pull

--- modules/weather.js
+++ modules/weather.js
@@ -1,28 +1,23 @@
 'use strict';
-let weatherData = require('./data/weather.json');
 
-function weather(req, res, next) {
+// REQUIRE
+let weatherData = process.env.WEATHER_API_KEY;
+const express = require('express');
+const axios = require('axios');
+//USE
+const app = express();
+function weather, async (req, res, next) {
+  const { lat, lon } = req.query;
   try {
-    let searchForCity = req.query.searchQuery;
-    if (searchForCity !== 'Seattle' && searchForCity !== 'Amman' && searchForCity !== 'Paris') {
-      next(res.error);
-    } else {
-      let cityChosen = weatherData.find(city => city.city_name === city);
-      let arr = [];
-      // array for forecast object
-      cityChosen.data.forEach(days => arr.push(new Forecast(days)));
-      res.send(arr);
-    }
-
+    let weatherUrl = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lang=en&lat=${lat}&lon=${lon}&days=3`;
+    
+    let weather = await axios.get(weatherData)
   } catch (error) {
-    (error);
+    next(error);
   }
 }
 
-app.use((error, req, res,) => {
-  res.status(500).send(error.message);
 
-});
 
 
 
@@ -35,4 +30,18 @@ class Forecast {
   }
 }
 
+app.use((error, req, res,) => {
+  res.status(500).send(error.message);
+});
 module.exports = weather;
+
+// let searchForCity = req.query.searchQuery;
+// if (searchForCity !== 'Seattle' && searchForCity !== 'Amman' && searchForCity !== 'Paris') {
+//   next(res.error);
+// } else {
+//   let cityChosen = weatherData.find(city => city.city_name === searchForCity);
+//   let arr = [];
+//   // array for forecast object
+//   cityChosen.data.forEach(days => arr.push(new Forecast(days)));
+//   res.send(arr);
+// }
